1028f23c417e21610e9401863223193ef321af57
Klemen Nagode <klemen.nagode@gmail.com>
2020-12-05 15:25:44 +0100

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "additional_laws", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+  create_table "additional_laws", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "description", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -48,38 +48,39 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "average_salaries", force: :cascade do |t|
+  create_table "average_salaries", id: :serial, force: :cascade do |t|
     t.decimal "net", precision: 15, scale: 2, default: "0.0"
     t.decimal "gross", precision: 15, scale: 2, default: "0.0"
-    t.string "country"
+    t.string "country", limit: 255
     t.integer "year"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "bank_transactions", force: :cascade do |t|
+  create_table "bank_transactions", id: :serial, force: :cascade do |t|
     t.decimal "amount"
     t.integer "user_id"
     t.boolean "is_processed"
     t.text "mail_body"
-    t.string "mail_subject"
-    t.string "mail_from"
+    t.string "mail_subject", limit: 255
+    t.string "mail_from", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "invoice_id"
+  end
+
+  create_table "expense_categories", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "nice_id", limit: 255
+    t.string "description", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "expense_categories", force: :cascade do |t|
-    t.string "name"
-    t.string "nice_id"
-    t.string "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "expenses", force: :cascade do |t|
+  create_table "expenses", id: :serial, force: :cascade do |t|
     t.integer "expense_category_id"
     t.decimal "price", precision: 15, scale: 2, default: "0.0"
-    t.string "currency", default: "EUR", null: false
+    t.string "currency", limit: 255, default: "EUR", null: false
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -88,131 +89,131 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.integer "payment_id"
   end
 
-  create_table "images", force: :cascade do |t|
-    t.string "name"
+  create_table "images", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "attachment_file_name"
-    t.string "attachment_content_type"
+    t.string "attachment_file_name", limit: 255
+    t.string "attachment_content_type", limit: 255
     t.integer "attachment_file_size"
     t.datetime "attachment_updated_at"
   end
 
-  create_table "invoice_items", force: :cascade do |t|
+  create_table "invoice_items", id: :serial, force: :cascade do |t|
     t.integer "invoice_id"
-    t.string "description"
+    t.string "description", limit: 255
     t.decimal "price", precision: 15, scale: 2, default: "0.0"
-    t.string "currency", default: "EUR", null: false
+    t.string "currency", limit: 255, default: "EUR", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "discount", default: false, null: false
   end
 
-  create_table "invoices", force: :cascade do |t|
+  create_table "invoices", id: :serial, force: :cascade do |t|
     t.integer "project_id"
     t.decimal "price", precision: 15, scale: 2, default: "0.0"
-    t.string "currency", default: "EUR", null: false
+    t.string "currency", limit: 255, default: "EUR", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "our_company_id"
-    t.string "invoice_number"
+    t.string "invoice_number", limit: 255
     t.datetime "sent_at"
     t.boolean "storno", default: false, null: false
     t.integer "due_days"
-    t.string "status"
+    t.string "status", limit: 255
     t.datetime "service_delivered_at"
     t.integer "additional_law_id"
-    t.string "before_table_text"
+    t.string "before_table_text", limit: 255
     t.integer "user_id"
   end
 
-  create_table "minimum_salaries", force: :cascade do |t|
+  create_table "minimum_salaries", id: :serial, force: :cascade do |t|
     t.decimal "net", precision: 15, scale: 2, default: "0.0"
     t.decimal "gross", precision: 15, scale: 2, default: "0.0"
-    t.string "country"
+    t.string "country", limit: 255
     t.integer "year"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "oauths", force: :cascade do |t|
+  create_table "oauths", id: :serial, force: :cascade do |t|
     t.integer "user_id"
-    t.string "provider"
-    t.string "uid"
+    t.string "provider", limit: 255
+    t.string "uid", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["provider", "uid"], name: "index_oauths_on_provider_and_uid"
   end
 
-  create_table "our_companies", force: :cascade do |t|
-    t.string "name"
-    t.string "reference_number"
-    t.string "address"
-    t.string "zip_code"
-    t.string "city"
-    t.string "country"
-    t.string "trr"
-    t.string "bank"
-    t.string "swift_bic_code"
-    t.string "contact_name"
-    t.string "contact_phone"
-    t.string "contact_email"
+  create_table "our_companies", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "reference_number", limit: 255
+    t.string "address", limit: 255
+    t.string "zip_code", limit: 255
+    t.string "city", limit: 255
+    t.string "country", limit: 255
+    t.string "trr", limit: 255
+    t.string "bank", limit: 255
+    t.string "swift_bic_code", limit: 255
+    t.string "contact_name", limit: 255
+    t.string "contact_phone", limit: 255
+    t.string "contact_email", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "registration_number"
-    t.string "vat_number"
+    t.string "registration_number", limit: 255
+    t.string "vat_number", limit: 255
   end
 
-  create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.string "slug"
+  create_table "pages", id: :serial, force: :cascade do |t|
+    t.string "title", limit: 255
+    t.string "slug", limit: 255
     t.text "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
     t.datetime "published_at"
-    t.string "status"
+    t.string "status", limit: 255
     t.boolean "visible_in_menu"
   end
 
-  create_table "partners", force: :cascade do |t|
-    t.string "name"
+  create_table "partners", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "address"
-    t.string "zip_code"
-    t.string "city"
-    t.string "country"
-    t.string "attention_to"
-    t.string "vat"
+    t.string "address", limit: 255
+    t.string "zip_code", limit: 255
+    t.string "city", limit: 255
+    t.string "country", limit: 255
+    t.string "attention_to", limit: 255
+    t.string "vat", limit: 255
     t.text "footer_text"
-    t.string "vat_number"
+    t.string "vat_number", limit: 255
   end
 
-  create_table "payments", force: :cascade do |t|
+  create_table "payments", id: :serial, force: :cascade do |t|
     t.integer "project_id"
     t.integer "our_company_id"
     t.integer "invoice_id"
     t.decimal "price", precision: 15, scale: 2, default: "0.0"
-    t.string "currency", default: "EUR", null: false
+    t.string "currency", limit: 255, default: "EUR", null: false
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "payed_at"
   end
 
-  create_table "projects", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+  create_table "projects", id: :serial, force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "description", limit: 255
     t.text "content"
-    t.string "url"
-    t.string "itunes_url"
-    t.string "google_url"
-    t.string "status"
+    t.string "url", limit: 255
+    t.string "itunes_url", limit: 255
+    t.string "google_url", limit: 255
+    t.string "status", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "image_file_name"
-    t.string "image_content_type"
+    t.string "image_file_name", limit: 255
+    t.string "image_content_type", limit: 255
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "partner_id"
@@ -220,13 +221,13 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.index ["status"], name: "index_projects_on_status"
   end
 
-  create_table "scrum_tasks", force: :cascade do |t|
+  create_table "scrum_tasks", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
-    t.string "description"
+    t.string "description", limit: 255
     t.time "time_to_do"
     t.time "time_used"
-    t.string "not_finished_or_overtime_justification"
+    t.string "not_finished_or_overtime_justification", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "scrum_id"
@@ -236,26 +237,26 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.integer "minutes_used", default: 0
   end
 
-  create_table "scrums", force: :cascade do |t|
+  create_table "scrums", id: :serial, force: :cascade do |t|
     t.date "date"
     t.time "finished_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "role"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", limit: 255
+    t.string "role", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string "password_digest"
+    t.string "password_digest", limit: 255
     t.boolean "deleted", default: false, null: false
     t.boolean "enabled", default: true, null: false
   end
 
-  create_table "vacations", force: :cascade do |t|
+  create_table "vacations", id: :serial, force: :cascade do |t|
     t.integer "user_id"
-    t.string "description"
+    t.string "description", limit: 255
     t.date "end_date"
     t.float "days"
     t.datetime "created_at"
@@ -263,7 +264,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_081045) do
     t.date "start_date"
   end
 
-  create_table "wanted_salaries", force: :cascade do |t|
+  create_table "wanted_salaries", id: :serial, force: :cascade do |t|
     t.decimal "net", precision: 15, scale: 2, default: "0.0"
     t.date "valid_from"
     t.date "valid_to"
