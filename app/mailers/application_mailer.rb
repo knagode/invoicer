e791fac75a46f3bec5e72b986e1d8f135bcd0ff6
Klemen <klemen.nagode@gmail.com>
2022-07-06 11:04:13 +0200

    fix double attachment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,11 +3,10 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def invoice_email(invoice)
-    mail(:subject => "Invoice #{invoice.invoice_number}", :to => invoice.project.partner.forward_invoice_to_email) do |format|
-      format.pdf do
-        @object = invoice
-        attachments["#{invoice.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "MindMap", :template => 'invoices/show.html.haml', :locals => {:object => invoice}))
-      end
-    end
+    @object = invoice
+
+    attachments["#{invoice.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(render_to_string(:pdf => "MindMap", :template => 'invoices/show.html.haml', :locals => {:object => invoice}))
+    
+    mail(:subject => "Invoice #{invoice.invoice_number}", :to => invoice.project.partner.forward_invoice_to_email)
   end
 end
